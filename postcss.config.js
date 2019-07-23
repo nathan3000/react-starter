b75fabe10a23331ee1c487af0cb953c25d9ed608
Nathan Fisher <nathan.fisher@stepstone.com>
2019-07-23 15:43:01 +0100

    Add purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,16 @@
-const tailwindcss = require('tailwindcss');
+const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./public/**/*.html", "./src/**/*.tsx"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')],
+  plugins: [
+    tailwindcss("./tailwind.config.js"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
