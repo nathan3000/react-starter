400147b06ed3250bbdc1a77e76354a954af789f9
Nathan Fisher <nathan.fisher@stepstone.com>
2019-07-23 14:36:04 +0100

    Sort out storybook TS errors

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-import { linkTo } from "@storybook/addon-links";
-
-import { Welcome } from "@storybook/react/demo";
 
 import App from "../components/App";
 
 import "../index.css";
-
-storiesOf("Welcome", module).add("to Storybook", () => (
-  <Welcome showApp={linkTo("App")} />
-));
 
 storiesOf("App", module).add("default", () => <App title="React Starter" />);
